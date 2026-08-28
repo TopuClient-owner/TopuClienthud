e384eb811c2dc74672787b97e9f5ff6948829971
TopuOrganization <betterechap@gmail.com>
2026-08-28 11:08:55 +0500

    Update TopuScreenEditor.java

--- 1.21.1/src/main/java/com/bettertoppi/topuhud/modmenu/TopuScreenEditor.java
+++ 1.21.1/src/main/java/com/bettertoppi/topuhud/modmenu/TopuScreenEditor.java
@@ -11,8 +11,7 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 
 public final class TopuScreenEditor extends Screen {
-
-```
+`
 private final Screen parent;
 private final TopuHudConfig config;
 
