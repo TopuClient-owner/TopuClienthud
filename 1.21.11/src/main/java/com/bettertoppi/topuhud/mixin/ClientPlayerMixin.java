141109238de58b37bf047929df14ed5e94dedd0b
TopuOrganization <topuclient@gmail.com>
2026-09-06 12:34:27 +0500

    Make Topu hit sound utility functional

--- 1.21.11/src/main/java/com/bettertoppi/topuhud/mixin/ClientPlayerMixin.java
+++ 1.21.11/src/main/java/com/bettertoppi/topuhud/mixin/ClientPlayerMixin.java
@@ -1,7 +1,10 @@
 package com.bettertoppi.topuhud.mixin;
 
+import com.bettertoppi.topuhud.config.ConfigManager;
 import com.bettertoppi.topuhud.hud.HudManager;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.sound.SoundEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,5 +15,9 @@ public final class ClientPlayerMixin {
     @Inject(method="attack",at=@At("HEAD"))
     private void topuhud$attack(CallbackInfo ci){
         HudManager.registerHit();
+        if(ConfigManager.get().utilityHitSound){
+            ClientPlayerEntity player=(ClientPlayerEntity)(Object)this;
+            player.playSound(SoundEvents.ENTITY_PLAYER_ATTACK_STRONG,0.75F,1.15F);
+        }
     }
 }
