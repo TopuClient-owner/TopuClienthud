bca426088c5e34faa1214342eb4ad776a506e51f
TopuOrganization <topuclient@gmail.com>
2026-09-06 19:31:00 +0500

    Keep all 1.21.x versions only on 1.21.x branch

---
+++ 1.21.1/src/main/java/com/bettertoppi/topuhud/config/TopuHudConfig.java
@@ -0,0 +1,23 @@
+package com.bettertoppi.topuhud.config;
+
+public final class TopuHudConfig {
+    public boolean armorHud=true, fpsCounter=true, pingDisplay=true, tpsDisplay=true;
+    public boolean cpsDisplay=true, comboCounter=true, totemCounter=true, potionEffects=true, potionCounter=true, gappleCounter=true;
+    public boolean armorWarning=true, enemyHealth=true, cooldown=true, blockOverlay=true, keystrokes=true, memory=true;
+    public boolean autoSprint=true, toggleSneak=true, editMode=false;
+
+    // Built-in Topu Utility modules (50 total)
+    public boolean utilityCoordinates=true, utilityDirection=true, utilitySpeed=true;
+    public boolean utilityJumpCounter=false, utilityFallDistance=false, utilitySprintStatus=false, utilityVelocity=false;
+    public boolean utilityCrosshair=false, utilityHitColor=false, utilityHitSound=false, utilityReach=false, utilityAttackIndicator=true;
+    public boolean utilityZoom=false, utilityFullbright=false, utilityFpsLimiter=false, utilityLowLatency=false, utilityCleanHud=false;
+    public boolean utilityMinimalParticles=false, utilityItemPhysics=false, utilityClock=false, utilityServer=false, utilityBiome=false;
+    public boolean utilityFacing=false, utilityChunkInfo=false, utilityLightLevel=false, utilityTargetInfo=false, utilityDurability=false;
+    public boolean utilityHeldItem=false, utilityGamemode=false, utilityDifficulty=false, utilityFpsGraph=false, utilitySession=false;
+
+    public int armorX=10, armorY=10, fpsX=10, fpsY=70, pingX=10, pingY=86, tpsX=10, tpsY=102;
+    public int cpsX=10, cpsY=118, comboX=10, comboY=134, totemX=10, totemY=150, potionX=10, potionY=166;
+    public int effectsX=10, effectsY=182, gappleX=10, gappleY=214, warningX=10, warningY=232;
+    public int enemyHealthX=10, enemyHealthY=250, cooldownX=10, cooldownY=270;
+    public int blockOverlayX=10, blockOverlayY=300, keystrokesX=10, keystrokesY=340, memoryX=10, memoryY=410;
+}
