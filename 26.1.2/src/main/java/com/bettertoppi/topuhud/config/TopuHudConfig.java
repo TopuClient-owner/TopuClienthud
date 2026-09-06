9e946af6bfee1dc58aff90685fa5bac51c67dd64
TopuOrganization <topuclient@gmail.com>
2026-09-06 18:56:56 +0500

    Add 26.1.2 TopuHUD config

---
+++ 26.1.2/src/main/java/com/bettertoppi/topuhud/config/TopuHudConfig.java
@@ -0,0 +1,6 @@
+package com.bettertoppi.topuhud.config;
+public final class TopuHudConfig {
+ public boolean armorHud=true,fpsCounter=true,pingDisplay=true,tpsDisplay=true,cpsDisplay=true,comboCounter=true,totemCounter=true,potionEffects=true,potionCounter=true,gappleCounter=true,armorWarning=true,enemyHealth=true,cooldown=true,blockOverlay=true,keystrokes=true,memory=true,autoSprint=true,toggleSneak=true,editMode=false;
+ public boolean utilityCoordinates=true,utilityDirection=true,utilitySpeed=true,utilityJumpCounter=false,utilityFallDistance=false,utilitySprintStatus=false,utilityVelocity=false,utilityCrosshair=false,utilityHitColor=false,utilityHitSound=false,utilityReach=false,utilityAttackIndicator=true,utilityZoom=false,utilityFullbright=false,utilityFpsLimiter=false,utilityLowLatency=false,utilityCleanHud=false,utilityMinimalParticles=false,utilityItemPhysics=false,utilityClock=false,utilityServer=false,utilityBiome=false,utilityFacing=false,utilityChunkInfo=false,utilityLightLevel=false,utilityTargetInfo=false,utilityDurability=false,utilityHeldItem=false,utilityGamemode=false,utilityDifficulty=false,utilityFpsGraph=false,utilitySession=false;
+ public int utilityX=12,utilityY=12;
+}
