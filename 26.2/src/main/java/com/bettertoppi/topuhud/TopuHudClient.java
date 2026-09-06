c68434e6faf726aade16efdaddb36552258b31db
TopuOrganization <topuclient@gmail.com>
2026-09-06 19:59:44 +0500

    Fix 26.2 key mapping API

--- 26.2/src/main/java/com/bettertoppi/topuhud/TopuHudClient.java
+++ 26.2/src/main/java/com/bettertoppi/topuhud/TopuHudClient.java
@@ -8,12 +8,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
 import net.minecraft.resources.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 public final class TopuHudClient implements ClientModInitializer {
     private static final KeyMapping.Category CATEGORY=KeyMapping.Category.register(Identifier.fromNamespaceAndPath("topuhud","main"));
-    private static final KeyMapping MENU=KeyMappingHelper.registerKeyMapping(new KeyMapping("key.topuhud.menu",InputConstants.Type.KEYSYM,InputConstants.KEY_RIGHT_SHIFT,CATEGORY));
+    private static final KeyMapping MENU=KeyMappingHelper.registerKeyMapping(new KeyMapping("key.topuhud.menu",InputConstants.Type.KEYSYM,GLFW.GLFW_KEY_RIGHT_SHIFT,CATEGORY));
     @Override public void onInitializeClient(){
         ConfigManager.load();
         TopuUtilityHud.initialize();
